3461e310ef16ea75a437cdf681b521e753350b30
mbb-macbook <mbb-macbook@80011046-MacBook-Pro.local>
2025-03-30 22:36:53 +0700

    rollback css and js for toogle button theme

--- assets/js/mode-change.js
+++ assets/js/mode-change.js
@@ -1,21 +1,39 @@
-// Change theme mode and save to cookie
+// change theme mode and save to cookie
 function changeMode() {
+    // detect body data-theme attribute
     let theme = document.body.getAttribute("data-theme");
+    // on first load, check if user has a preference in cookie
     const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
     if (theme === null) {
         theme = isDark ? "dark" : "light";
     }
-
-    // Toggle theme
+    // toggle theme
     theme = theme === "dark" ? "light" : "dark";
     document.body.setAttribute("data-theme", theme);
-
-    // Save theme preference to cookie
+    // save theme preference to cookie for 1 year and set SameSite to all pages
     document.cookie = `theme=${theme}; max-age=31536000; SameSite=Lax; path=/`;
 }
 
-// Get cookie by name
+// add event load to check cookie for theme preference and set the theme
+window.addEventListener("load", () => {
+    // check if user has a preference in cookie
+    const theme = getCookie("theme");
+    if (theme) {
+        document.body.setAttribute("data-theme", theme);
+    }
+});
+// auto change theme based data-theme attribute
+function autoChangeMode() {
+    // detect body data-theme attribute
+    let theme = document.body.getAttribute("data-theme");
+    // on first load, check if user has a preference in cookie
+    const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
+    if (theme === null) {
+        theme = isDark ? "dark" : "light";
+    }
+}
+
+// get cookie by name
 function getCookie(name) {
     const value = `; ${document.cookie}`;
     const parts = value.split(`; ${name}=`);
@@ -23,27 +41,10 @@ function getCookie(name) {
         return parts.pop().split(";").shift();
     }
 }
-
-// Auto-change theme based on user preference
-function autoChangeMode() {
-    let theme = document.body.getAttribute("data-theme");
-    const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-
-    if (theme === null) {
-        theme = isDark ? "dark" : "light";
-    }
-}
-
-// Set theme on page load
-window.addEventListener("load", () => {
-    const theme = getCookie("theme");
-    if (theme) {
-        document.body.setAttribute("data-theme", theme);
-    }
-});
-
-// Ensure script loads before other scripts
+// make it priority to load before other scripts
 document.addEventListener("DOMContentLoaded", function () {
+    // add event listener to mode button
     document.getElementById("mode").addEventListener("click", changeMode);
+    // add event listener to auto change mode
     window.matchMedia("(prefers-color-scheme: dark)").addEventListener("change", autoChangeMode);
 });
